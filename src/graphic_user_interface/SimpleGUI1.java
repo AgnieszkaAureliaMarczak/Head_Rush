9c83e93cb5c69549e7aab5f38b0c9ac0c786c4d1
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-03-21 23:13:59 +0100

    JButton can be clicked

--- src/graphic_user_interface/SimpleGUI1.java
+++ src/graphic_user_interface/SimpleGUI1.java
@@ -1,15 +1,29 @@
 package graphic_user_interface;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleGUI1 {
+public class SimpleGUI1 implements ActionListener {
+    JButton button;
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Click Me");
-        frame.getContentPane().add(button);
+        SimpleGUI1 appGUI = new SimpleGUI1();
+        appGUI.letsStart();
+    }
 
+    public void letsStart(){
+        JFrame frame = new JFrame();
+        button = new JButton("Clcik Me");
+        button.addActionListener(this);
+        frame.getContentPane().add(button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700,600);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("I've been clicked");
     }
 }
